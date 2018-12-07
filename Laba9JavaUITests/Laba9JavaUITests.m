071b3e3ae5f7e4c22294fcf3e2692e8810a3ae66
Dima <dima@Dimas-Mac.local>
2018-12-07 22:23:18 +0300

    Initial Commit

---
+++ Laba9JavaUITests/Laba9JavaUITests.m
@@ -0,0 +1,40 @@
+//
+//  Laba9JavaUITests.m
+//  Laba9JavaUITests
+//
+//  Created by Dima  on 12/7/18.
+//  Copyright © 2018 Dima . All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Laba9JavaUITests : XCTestCase
+
+@end
+
+@implementation Laba9JavaUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
